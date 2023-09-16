bb2f5cfe9513aa782479971b19841f7ad79e7b34
Sahed Wahedi <sahed.w@hotmail.de>
2023-09-16 18:08:16 +0200

    remove id service

---
+++ backend/src/main/java/com/github/sahedw/backend/models/FatwaService.java
@@ -0,0 +1,18 @@
+package com.github.sahedw.backend.models;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FatwaService {
+
+    private final FatwaRepo fatwaRepo;
+
+    public List<Fatwa> getAllFatwas() {
+        System.out.println(fatwaRepo.findAll());
+        return fatwaRepo.findAll();
+    }
+}
