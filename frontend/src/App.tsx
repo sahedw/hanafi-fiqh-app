4e317a6ccdb36b9fe14d932201f8238ffb8d0253
Sahed Wahedi <sahed.w@hotmail.de>
2023-09-26 00:50:49 +0200

    setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <>
-            <h1>Objects:</h1>
+            <h1>Objects test again:</h1>
             {fatwas.map((fatwa) => {
                 return (
                     <>
